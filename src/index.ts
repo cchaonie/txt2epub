374505fd323c8246ad3cf22b2e5ead4d0fbde627
fightant1w1ll <nc_8093@163.com>
2022-12-31 10:52:40 +0800

    handle section

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ const [, , sourceName, targetName, targetFolder] = process.argv;
 const cwd = process.cwd();
 
 const filePath = `${sourceName}.txt`;
-const pageTemplatePath = './templates/page.html';
-
 const fileOutPath = `${targetFolder}/${targetName}`;
 
-const titlePattern = /^第\d*章/;
+const pageTemplatePath = './templates/page.html';
+const sectionTemplatePath = './templates/section.html';
+
+const pageTitlePattern = /^第[\d|一|二|三|四|五|六|七|八|九|十|百]*章/;
+const sectionTitlePattern = /^第[\d|一|二|三|四|五|六|七|八|九|十|百]*[卷|集]/;
 
 const fileContent = await fs.readFile(path.resolve(cwd, filePath), {
   encoding: 'utf-8',
@@ -21,40 +23,62 @@ const pageTemplate = await fs.readFile(path.resolve(cwd, pageTemplatePath), {
   encoding: 'utf-8',
 });
 
-const fileContentInLines = fileContent.split('\n').filter(l => !!l);
-
-let currentTitle = '请输入标题';
-let currentContent: string[] = [];
+const sectionTemplate = await fs.readFile(
+  path.resolve(cwd, sectionTemplatePath),
+  {
+    encoding: 'utf-8',
+  }
+);
 
 const pages: string[] = [];
 
 async function parseContent() {
+  let currentTitle = targetName; // the initial title should be the name of the book
+  let currentContent: string[] = [];
+
+  const fileContentInLines = fileContent.split(/\r?\n/).filter(l => !!l);
+
+  console.log(fileContentInLines)
   _.each(fileContentInLines, line => {
-    if (titlePattern.test(line)) {
-      // now we should generate a new page
-      const pageContent = pageTemplate
-        .replace('{{title}}', currentTitle)
-        .replace(
-          '{{content}}',
-          `<h2>${currentTitle}</h2>` + currentContent.join('\n')
-        );
+    if (sectionTitlePattern.test(line) || pageTitlePattern.test(line)) {
+      // now we see a section or a new page, we should create a new page if `currentContent` is not empty
+      if (currentContent.length) {
+        const pageContent = pageTemplate
+          .replace('{{title}}', currentTitle)
+          .replace(
+            '{{content}}',
+            currentTitle === targetName
+              ? `<h1>${currentTitle}</h1>`
+              : `<h2>${currentTitle}</h2>` + currentContent
+          );
 
-      pages.push(pageContent);
+        pages.push(pageContent);
 
-      currentContent = [];
-      currentTitle = line;
+        currentContent = [];
+      }
+
+      if (sectionTitlePattern.test(line)) {
+        // console.log('section: ', line);
+        // now we should generate a new section
+        const sectionContent = sectionTemplate
+          .replace('{{title}}', line)
+          .replace('{{section}}', `<h1>${line}</h1>`);
+
+        pages.push(sectionContent);
+      } else {
+        // console.log('title: ', line);
+        currentTitle = line;
+      }
     } else {
       currentContent.push(`<p>${line}</p>`);
     }
   });
 
+  // Once we add the last page title, we will not have the chance to create a new page for it
   if (currentContent.length) {
     const pageContent = pageTemplate
       .replace('{{title}}', currentTitle)
-      .replace(
-        '{{content}}',
-        `<h2>${currentTitle}</h2>` + currentContent.join('\n')
-      );
+      .replace('{{content}}', `<h2>${currentTitle}</h2>`);
 
     pages.push(pageContent);
   }
