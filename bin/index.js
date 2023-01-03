6996dd9b825eef543dd6c819fa34c3c6dfcfd7c3
Chris Nie <nc_8093@163.com>
2023-01-03 16:44:55 +0800

    Call the main function

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-const run = require('../lib/index');
+const { default: run } = require('../lib/index');
+
+console.log('Running......');
+
+run()
+  .then(() => 'Successful')
+  .catch(e => console.error('Failed:', e));
