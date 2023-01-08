9114ae37230ae80587202b7b99c78454d23c2e0f
fightant1w1ll <nc_8093@163.com>
2023-01-08 12:32:59 +0800

    Add unit test file

---
+++ src/mocks/fileContent.ts
@@ -0,0 +1,26 @@
+export default `
+HelloWorld
+第一卷 Hello
+第一章 h
+hhhhh
+第二章 e
+eeeee
+第三章 l1
+l1l1l1l1l1
+第四章 l2
+l2l2l2l2l2
+第五章 o
+ooooo
+
+第二卷 World
+第一章 w
+wwwww
+第二章 o
+ooooo
+第三章 r
+rrrrr
+第四章 l
+lllll
+第五章 d
+ddddd
+`;
