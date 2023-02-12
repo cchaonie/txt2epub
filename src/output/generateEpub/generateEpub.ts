aa02a4b85ef94281addffd273954c357bd90374c
cchaonie <nc_8093@163.com>
2023-02-12 14:58:55 +0800

    throw error when failed

--- src/output/generateEpub/generateEpub.ts
+++ src/output/generateEpub/generateEpub.ts
@@ -24,9 +24,9 @@ export default async function generateEpub({
   try {
     const epub = new Epub(options);
     await epub.render();
-
     console.log('Ebook Generated Successfully!');
   } catch (error) {
     console.error('Failed to generate Ebook because of ', error);
+    throw error;
   }
 }
