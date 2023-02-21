e1258903f9c95bf8289180b8852af93bf7a0777e
Chris Nie <nc_8093@163.com>
2023-02-21 16:28:36 +0800

    not compile packages in node_modules

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,31 +1,9 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
-import replace from 'rollup-plugin-re';
-import isBuiltin from 'is-builtin-module';
 
 const shared = {
   plugins: [
-    nodeResolve({
-      resolveOnly: (module) =>
-        module === 'string_decoder' || !isBuiltin(module),
-      preferBuiltins: false,
-    }),
-    replace({
-      patterns: [
-        {
-          match: /formidable(\/|\\)lib/,
-          test: 'if (global.GENTLY) require = GENTLY.hijack(require);',
-          replace: '',
-        },
-        {
-          match: /'string_decoder\/'/,
-          test: "'string_decoder/'",
-          replace: "'string_decoder/lib/string_decoder.js'",
-        },
-      ],
-    }),
     commonjs(),
     json(),
     typescript(),
